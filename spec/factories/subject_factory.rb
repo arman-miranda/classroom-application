c0b7a408c32fbd44554f0349e6e840d734d2838f
armanmiranda <arman.miranda@adish.co.jp>
2017-07-31 13:59:43 +0800

    added factory girl

---
+++ spec/factories/subject_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :subject do
+    name "Computer Fundamentals"
+    code "CS101"
+  end
+end
--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -3,15 +3,18 @@ require 'rails_helper'
 RSpec.describe Subject, type: :model do
   describe "initialization" do
 
-    let (:subject) { Subject.new }
+    let (:subject) { FactoryGirl.create(:subject, name: nil, code: nil) }
 
     it "has a name that can't be blank" do
       expect(subject.name).to be_falsy
+
       subject.name = "Computer Fundamentals"
       expect(subject.name).to be_truthy
     end
 
     it "has an optional subject code" do
+      expect(subject.code).to be_falsy
+
       subject.code = "CS101"
       expect(subject.code).to be_truthy
     end
