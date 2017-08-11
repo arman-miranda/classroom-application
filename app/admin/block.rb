3f1c3add74a95342bddca14054695b666f6bfa59
armanmiranda <arman.miranda@adish.co.jp>
2017-08-11 12:48:19 +0800

    add functionality where the block can add its advisory teacher

--- app/admin/block.rb
+++ app/admin/block.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Block do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :name, :year_level
+permit_params :name, :year_level, :user_id
 #
 # or
 #
@@ -11,5 +11,23 @@ permit_params :name, :year_level
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
+  index do
+    selectable_column
+    id_column
+    column :year_level
+    column :name
+    column :advisory_teacher
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :year_level, as: :select, collection: (1..4)
+      f.input :name
+      f.input :advisory_teacher, collection: User.with_role(:teacher)
+    end
+    f.actions
+  end
 
 end
