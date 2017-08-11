f8dc067c73ca22cb6a071409b523c03aaf82f133
armanmiranda <arman.miranda@adish.co.jp>
2017-08-11 13:02:11 +0800

    add Student and Teacher resource in active admin

--- app/admin/teacher.rb
+++ app/admin/teacher.rb
@@ -1,24 +1,15 @@
-ActiveAdmin.register User do
-  menu :if => proc{ can? :manage, User} 
-  permit_params  :email, :password, :password_confirmation, :first_name, :last_name, :birthdate, :address, role_ids: []
-  controller do
-    def scoped_collection
-      User.with_role :teacher
-    end
-  end
-
+ActiveAdmin.register User, as: "Teachers" do
+  permit_params  :email, :password, :password_confirmation, :first_name, :last_name, :birthdate, :address, subject_ids: []
+  
   index do
     selectable_column
     id_column
     column :first_name
     column :last_name
     column :email
-    column :roles do |role|
-      role.roles.map { |role| role.name }.join(", ")
+    column :subjects do |subject|
+      subject.subjects.map(&:name).join(", ")
     end
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
     actions
   end
 
@@ -40,5 +31,4 @@ ActiveAdmin.register User do
     end
     f.actions
   end
-
 end
