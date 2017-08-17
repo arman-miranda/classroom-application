bbadbd4bca11a32d1679af4116ec9aa185d30061
armanmiranda <arman.miranda@adish.co.jp>
2017-08-17 15:39:51 +0800

    add flash messages with stylings

--- app/assets/javascripts/turbolinks_transitions.js
+++ app/assets/javascripts/turbolinks_transitions.js
@@ -1,6 +1,15 @@
 document.addEventListener('turbolinks:load', function() {
           document.getElementById('primary-content').className += 'animated slideInLeft';
+          var alertEle = document.getElementById('alert')
+          var noticeEle = document.getElementById('notice')
+
+          if(noticeEle!= null){
+            document.getElementById('notice').className += 'animated slideInDown';
+          }else if(alertEle!= null){
+            document.getElementById('alert').className += 'animated slideInDown';
+          }
 });
 document.addEventListener('turbolinks:visit', function() {
           document.getElementById('primary-content').className += 'animated slideOutLeft';
 });
+
